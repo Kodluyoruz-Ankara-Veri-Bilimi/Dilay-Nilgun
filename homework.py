858389b07b25f166a0f6d9e8c9b381fd46410620
dilaynilgun <62816304+dilaynilgun@users.noreply.github.com>
2020-07-11 07:31:47 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,7 +1,7 @@
 import pandas as pd
-        import numpy as np
-        import seaborn as sns
-        import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 class Information():
     def __init__(self,file_name):
